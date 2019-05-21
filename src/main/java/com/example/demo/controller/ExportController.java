8d17f22c81e21b07b853ee40e984abce97e7ea5e
j.camus <jcamus@pyxis-sas.com>
2019-05-21 12:40:36 +0200

    v1

--- src/main/java/com/example/demo/controller/ExportController.java
+++ src/main/java/com/example/demo/controller/ExportController.java
@@ -2,6 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Client;
 import com.example.demo.service.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +37,28 @@ public class ExportController {
         List<Client> allClients = clientService.findAllClients();
         LocalDate now = LocalDate.now();
         writer.println("Id;Nom;Prenom;Date de Naissance;Age");
-
+        for (Client client : allClients) {
+            writer.println(client.getId() + " ; " + client.getNom() + " ; " + client.getPrenom() + " ; " + client.getDateNaissance());
+        }
     }
-}
+    @GetMapping("/clients/xlsx")
+    public void clientsXLSX(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("application/vnd.ms-exel");
+        response.setHeader("Content-Disposition", "attachment; filename=\"clients.xlsx\"");
+        List<Client> allClients = clientService.findAllClients();
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Clients");
+        Row headerRow = sheet.createRow(0);
+
+        Cell cellPrenom = headerRow.createCell(0);
+        cellPrenom.setCellValue("Prénom");
+
+        int iRow = 1;
+        for(Client client : allClients) {
+            Row row = sheet.createRow(iRow);
+            Cell id = row.createCell(0);
+            id.setCellValue(client.getId());
+
+            Cell prenom = row.createCell(1);
+            prenom.setCellVal
